295ebdfa6f505d604b961cfc6ee3021a0ec7beb5
Karol Bisztyga <karolbisztyga@Karols-MacBook-Pro.local>
2024-07-20 10:37:00 +0200

    editing

--- app/(tabs)/profile/components/Contact.tsx
+++ app/(tabs)/profile/components/Contact.tsx
@@ -1,70 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { generateUserData } from '../../../mocks/userMocker';
 import { UserData } from '../../../types';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import * as Clipboard from 'expo-clipboard';
+import PersonalDataItem from './PersonalDataItem';
 
 const Contact = () => {
   const [userData, setUserData] = useState<UserData | null>(null);
-  const [editing, setEditing] = useState<Record<string, boolean>>({});
 
   useEffect(() => {
     const newUserData = generateUserData();
     setUserData(newUserData);
-
-    const newEditing: Record<string, boolean> = {};
-    Object.keys(newUserData || {}).forEach((k) => {
-      console.log('loop', k, newUserData && newUserData[k as keyof UserData]);
-      if (newUserData && newUserData[k as keyof UserData]) {
-        newEditing[k] = false;
-      }
-    });
-    setEditing(newEditing);
   }, []);
 
-  useEffect(() => {
-    console.log('user', userData);
-    console.log('editing', editing);
-  }, [editing, userData]);
+  console.log('user data', userData);
 
   return (
     <View style={styles.container}>
       {userData &&
-        Object.keys(userData).map((key, index) => {
-          const value = userData && userData[key as keyof UserData];
+        Object.keys(userData).map((name, index) => {
+          const value = userData && userData[name as keyof UserData];
           if (!value) {
             return null;
           }
-          return (
-            <View style={styles.constactItemWrapper} key={index}>
-              <Text style={styles.constactItemTitle}>{key}</Text>
-              <Text style={styles.constactItemValue}>{value}</Text>
-
-              <TouchableOpacity
-                style={styles.contactItemIconWrapper}
-                activeOpacity={1}
-                onPress={() => {
-                  const newEditing = { ...editing };
-                  newEditing[key] = true;
-                  setEditing(newEditing);
-                }}
-              >
-                <FontAwesome size={20} name="pencil" />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={styles.contactItemIconWrapper}
-                activeOpacity={1}
-                onPress={async () => {
-                  await Clipboard.setStringAsync(value);
-                  Alert.alert('Copied to Clipboard', value);
-                }}
-              >
-                <FontAwesome size={20} name="copy" />
-              </TouchableOpacity>
-            </View>
-          );
+          return <PersonalDataItem name={name} initialValue={value} index={index} key={index} />;
         })}
     </View>
   );
@@ -72,23 +30,6 @@ const Contact = () => {
 
 const styles = StyleSheet.create({
   container: {},
-  constactItemWrapper: {
-    margin: 10,
-    flexDirection: 'row',
-  },
-  constactItemTitle: {
-    padding: 10,
-    flex: 2,
-  },
-  constactItemValue: {
-    padding: 10,
-    flex: 5,
-  },
-  contactItemIconWrapper: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
 });
 
 export default Contact;
---
+++ app/(tabs)/profile/components/PersonalDataItem.tsx
@@ -0,0 +1,153 @@
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, Alert, TextInput } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as Clipboard from 'expo-clipboard';
+
+const validateValue = (value: string) => {
+  if (!value) {
+    return false;
+  }
+  return true;
+};
+
+const StandardTools = ({
+  value,
+  setEditing,
+}: {
+  value: string;
+  setEditing: (_: boolean) => void;
+}) => {
+  return (
+    <>
+      <TouchableOpacity
+        style={styles.contactItemIconWrapper}
+        activeOpacity={1}
+        onPress={() => {
+          setEditing(true);
+        }}
+      >
+        <FontAwesome size={20} name="pencil" />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.contactItemIconWrapper}
+        activeOpacity={1}
+        onPress={async () => {
+          await Clipboard.setStringAsync(value);
+          Alert.alert('Copied to Clipboard', value);
+        }}
+      >
+        <FontAwesome size={20} name="copy" />
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const EditingTools = ({
+  editingValue,
+  setValue,
+  setEditing,
+}: {
+  editingValue: string;
+  setValue: (_: string) => void;
+  setEditing: (_: boolean) => void;
+}) => {
+  return (
+    <>
+      <TouchableOpacity
+        style={styles.contactItemIconWrapper}
+        activeOpacity={1}
+        onPress={() => {
+          // todo validation
+          if (!validateValue(editingValue)) {
+            return;
+          }
+          setEditing(false);
+          setValue(editingValue);
+        }}
+      >
+        <FontAwesome size={20} name="check" />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.contactItemIconWrapper}
+        activeOpacity={1}
+        onPress={async () => {
+          setEditing(false);
+        }}
+      >
+        <FontAwesome size={20} name="ban" />
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const PersonalDataItem = ({
+  name,
+  initialValue,
+  index,
+}: {
+  name: string;
+  initialValue: string;
+  index: number;
+}) => {
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState(initialValue);
+  const [editingValue, setEditingValue] = useState(value);
+
+  useEffect(() => {
+    if (editing) {
+      setEditingValue(value);
+    }
+  }, [editing]);
+
+  return (
+    <View style={styles.container} key={index}>
+      <Text style={styles.constactItemTitle}>{name}</Text>
+      {editing ? (
+        <TextInput
+          value={editingValue}
+          onChangeText={setEditingValue}
+          style={[styles.constactItemValue, styles.editingTextInput]}
+        />
+      ) : (
+        <Text style={styles.constactItemValue}>{value}</Text>
+      )}
+      {editing ? (
+        <EditingTools
+          value={value}
+          editingValue={editingValue}
+          setValue={setValue}
+          setEditing={setEditing}
+        />
+      ) : (
+        <StandardTools value={value} setEditing={setEditing} />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    flexDirection: 'row',
+  },
+  constactItemTitle: {
+    padding: 10,
+    flex: 2,
+  },
+  constactItemValue: {
+    padding: 10,
+    flex: 5,
+  },
+  editingTextInput: {
+    backgroundColor: '#bdbda9',
+  },
+  contactItemIconWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default PersonalDataItem;
