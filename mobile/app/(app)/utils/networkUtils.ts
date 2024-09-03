144ed254d8e757ff5aa4789f92721425711bdbd5
Karol Bisztyga <karolbisztyga@Karols-MacBook-Pro.local>
2024-09-03 14:59:13 +0200

    deployed and connected to the server from mobile

--- mobile/app/(app)/utils/networkUtils.ts
+++ mobile/app/(app)/utils/networkUtils.ts
@@ -1,3 +1,5 @@
 export const getServerAddress = () => {
-  return `${process.env.EXPO_PUBLIC_SERVER_HOST}:${process.env.EXPO_PUBLIC_SERVER_PORT}`;
+  return process.env.EXPO_PUBLIC_ENV_ID === 'PROD'
+    ? `${process.env.EXPO_PUBLIC_SERVER_HOST}`
+    : `${process.env.EXPO_PUBLIC_SERVER_HOST}:${process.env.EXPO_PUBLIC_SERVER_PORT}`;
 };
