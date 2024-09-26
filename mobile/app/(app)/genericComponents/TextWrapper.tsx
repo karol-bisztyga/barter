ffccfb7561ff2984986731e864f74952393b92e4
Karol Bisztyga <karolbisztyga@Karols-MacBook-Pro.local>
2024-09-26 14:35:47 +0200

    change font in text

--- mobile/app/(app)/genericComponents/TextWrapper.tsx
+++ mobile/app/(app)/genericComponents/TextWrapper.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 import { StyleSheet, Text, TextProps } from 'react-native';
 import { FONT_COLOR } from '../constants';
+import { useFonts } from 'expo-font';
 
 interface TextWrapperProps extends TextProps {}
 
 const TextWrapper = ({ ...props }: TextWrapperProps) => {
+  const [loadedFonts] = useFonts({
+    RokkittRegular: require('../../../assets/fonts/Rokkitt-Regular.ttf'),
+  });
+
+  if (!loadedFonts) {
+    return null;
+  }
+
   return <Text {...props} style={[props['style'] ? props['style'] : {}, styles.text]} />;
 };
 
 const styles = StyleSheet.create({
   text: {
     color: FONT_COLOR,
-    backgroundColor: '#006676',
-    width: '100%',
+    fontFamily: 'RokkittRegular',
   },
 });
 
