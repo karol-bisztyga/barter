ef34c9dfed3757e01badbc175d6ab0064e0c7971
Karol Bisztyga <karolbisztyga@Karols-MacBook-Pro.local>
2024-11-11 11:25:16 +0100

    remove redundant scrrens from onboarding

--- mobile/app/(app)/onboarding/_layout.tsx
+++ mobile/app/(app)/onboarding/_layout.tsx
@@ -14,9 +14,6 @@ export default function Layout() {
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="stage_1" options={{ headerShown: false }} />
-      <Stack.Screen name="stage_2" options={{ headerShown: false }} />
-      <Stack.Screen name="stage_3" options={{ headerShown: false }} />
     </Stack>
   );
 }
