2e222939fb7c9a49fab2fc9465615652724674bd
Karol Bisztyga <karolbisztyga@Karols-MacBook-Pro.local>
2025-01-14 13:52:56 +0100

    fix missing image on frontend

--- mobile/app/(app)/genericComponents/ImageWrapper.tsx
+++ mobile/app/(app)/genericComponents/ImageWrapper.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Image, ImageProps, Platform } from 'react-native';
+import { Image, ImageProps, Platform, View } from 'react-native';
 import { EXPO_PUBLIC_ENV_ID, EXPO_PUBLIC_SERVER_HOST_IP } from '../utils/envs';
+import { hexToRgbaString } from '../utils/harmonicColors';
+import { TAB_BAR_BACKGROUND_COLOR } from '../constants';
 
 type MyImageProps = ImageProps & {
   uri: string;
@@ -10,7 +12,19 @@ const ImageWrapper = ({ uri, ...props }: MyImageProps) => {
   if (EXPO_PUBLIC_ENV_ID === 'LOCAL' && Platform.OS === 'android' && EXPO_PUBLIC_SERVER_HOST_IP) {
     uri = uri.replace('http://localhost', EXPO_PUBLIC_SERVER_HOST_IP);
   }
-  return <Image source={{ uri }} {...props} />;
+  const style = props.style || {};
+  delete props.style;
+  return (
+    <View
+      style={[
+        { backgroundColor: hexToRgbaString(TAB_BAR_BACKGROUND_COLOR, 0.7) },
+        style,
+        { overflow: 'hidden' },
+      ]}
+    >
+      <Image source={{ uri }} {...props} style={{ width: '100%', height: '100%' }} />
+    </View>
+  );
 };
 
 export default ImageWrapper;
